01204760bb220433b4c6bb4faca3b3f770bebcfa
Erlinis Quintana <erlinis.quintana@tourlane.com>
2020-03-29 20:22:18 +0200

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  env: {
+    HOST_NAME: process.env.HOST_NAME,
+  },
+}
---
+++ pages/blog/[slug].js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import Axios from 'axios';
+
+export default function Item({post}) {  
+  if (Object.keys(post).length === 0) {
+    return null;
+  }
+  
+  return (
+    <React.Fragment>
+      <h1>
+        {post.title.rendered}
+      </h1>
+      <div dangerouslySetInnerHTML={{ __html: post.content.rendered }}></div>
+    </React.Fragment>
+  );
+}
+
+export function getStaticPaths(){
+  return Axios
+    .get(`${process.env.HOST_NAME}/wp-json/wp/v2/posts`)
+    .then(
+      response => {
+        const paths = response.data.map(post => `/blog/${post.slug}`);
+
+        return {
+          paths: paths,
+          fallback: false
+        };
+    });
+}
+
+export function getStaticProps({params}){
+  const { slug } = params;
+  return Axios.get(`${process.env.HOST_NAME}/wp-json/wp/v2/posts/?slug=${slug}`)
+  .then(
+    response => {
+      return {
+        props: {
+          post: response.data[0] || {},
+        }
+      }
+    }
+  )
+}
